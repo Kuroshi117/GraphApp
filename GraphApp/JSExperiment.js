0b91c19578e01ab6d16e572a34fc9a4dbc0588f2
Kuroshi117 <phoebehana4@gmail.com>
2018-11-15 19:23:19 -0600

    Added a dummy mesh, and childed camera to it. Set controls to dummy. Continue to work on bounding boxes.

--- GraphApp/JSExperiment.js
+++ GraphApp/JSExperiment.js
@@ -2,8 +2,13 @@
 var camera, scene, renderer;
 var geometry, material, mesh;
 var ambientLight, pointLight, shadowMaterial;
-var controls;
+var controls, playMesh;
+var playBound, sphereBound; 
+
+
 var clock = new THREE.Clock();
+
+
 
 init();
 animate();
@@ -11,10 +16,16 @@ animate();
 function init() {
 
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 1000);
-    camera.position.set(0, 30, 50);
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    camera.position.set(0, 5, 20);
+
+    playMesh = new THREE.Mesh(new THREE.CubeGeometry(5, 5, 5, 1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff0000 }));//dummy mesh
+    playMesh.position.set(0, 0, 0);
+    playMesh.add(camera);
+    playBound = new THREE.Box3(playMesh.geometry.min, playMesh.geometry.max);
 
     scene = new THREE.Scene();
+
+    scene.add(playMesh);
 
     renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -38,18 +49,18 @@ function init() {
     geometry = new THREE.SphereGeometry(400, 32, 32);
     material = new THREE.MeshStandardMaterial({
         color: 0xe56baf8,
-        shading: THREE.SmoothShading,
+        shading: THREE.FlatShading,//change to SmoothShading
         metalness: 0,
         roughness: 0.0,
         side: THREE.BackSide
     });
     mesh = new THREE.Mesh(geometry, material);
     mesh.receiveShadow = true;
+    sphereBound = new THREE.Sphere(mesh.position, mesh.geometry.radius);
+
     scene.add(mesh);
 
-    
-
-    controls = new THREE.FlyControls(camera);
+    controls = new THREE.FlyControls(playMesh);
     controls.movementSpeed = 2;
     controls.domElement = renderer.domElement;
     controls.rollSpeed = Math.PI / 150;
@@ -62,6 +73,9 @@ function update() {
     controls.update(1);
     setTimeout(update, 16);
     draw();
+
+    //keep player in sphere
+    
 }
 function draw() {
     renderer.render(scene, camera);
@@ -70,10 +84,6 @@ function draw() {
 function animate() {
 
     requestAnimationFrame(animate);
-
-    //mesh.rotation.x += 0.01;
-    //mesh.rotation.y += 0.02;
-
     renderer.render(scene, camera);
  
 
